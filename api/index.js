5b756ce3e87798b3c956cf225f2608bdbde28874
saurav kumar rajak <sauravrajak@outlook.com>
2024-02-18 22:09:48 +0530

    install express and create db connection

---
+++ api/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+
+const URL = process.env.MONGOURL;
+const app = express();
+mongoose
+  .connect(URL)
+  .then(() => {
+    console.log("DB is connected");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+app.use(express.json());
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
+});
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello Express </h1>`);
+});
